7150a26181d6c76c1913fb60ef8c3c097cebafd8
Vikas <vsvikassvs@gmail.com>
2021-12-10 16:17:38 +0530

    used validate

--- functions/parse.js
+++ functions/parse.js
@@ -5,29 +5,28 @@ module.exports = (ajv) => {
     ajv.addKeyword({
         keyword: 'parse',
         type: 'string',
-        compile: (parse, { format }) => {
-            if (parse) {
-                return (data, { parentData, parentDataProperty }) => {
-                    switch (format) {
-                    case 'objectId':
-                        parentData[ parentDataProperty ] = ObjectId(data);
-                        break;
-                    case 'date':
-                        parentData[ parentDataProperty ] = new Date(data).toString();
-                        break;
-                    case 'date-time':
-                        parentData[ parentDataProperty ] = new Date(data);
-                        break;
-                    default:
-                        break;
-                    }
-                    return true;
-                };
+        validate: function validate(parse, data, { format }, { parentData, parentDataProperty }) {
+            let returnValue = true;
+
+            switch (format) {
+            case 'objectId':
+                parentData[ parentDataProperty ] = ObjectId(data);
+                break;
+            case 'date':
+                parentData[ parentDataProperty ] = new Date(data).toString();
+                break;
+            case 'date-time':
+                parentData[ parentDataProperty ] = new Date(data);
+                break;
+            default:
+                validate.errors = [{ keyword: 'parse', message: 'value should be valid format or format should be in [objectId, date, date-time].', params: { keyword: 'parse' } }];
+                returnValue = false;
+                break;
             }
-            return () => false;
+            return returnValue;
         },
         modifying: true,
-        errors: false,
+        errors: true,
         metaSchema: {
             type: 'boolean',
             const: true,
